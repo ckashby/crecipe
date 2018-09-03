902b0ae6e4cd6a447c08b260e588e5c6413e5228
ckashby <clay.ashby@gmail.com>
2018-09-03 09:26:01 -0700

    Recipe controller CRUD

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,22 @@
 class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @recipes = Recipe.all.order(:name)
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save?
+      redirect_to root_path, notice: 'Recipe successfully created'
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,8 +26,26 @@ class RecipesController < ApplicationController
   end
 
   def update
+    if @recipe.update(recipe_params)
+      redirect_to root_path, notice: 'Recipe successfully updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: 'Recipe successfully deleted'
   end
+
+    private
+
+    def find_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+      params.require(:recipe).permit(:title)
+    end
+
 end
